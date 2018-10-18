22e109d70b331451dc4666f8ff927db19bab8528
AndrewKanaew <andrewrook-media@yandex.ru>
2018-10-18 12:33:17 +0300

    Create projects

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+
+import VueMaterial from "vue-material";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default-dark.css";
+
+Vue.config.productionTip = false;
+Vue.use(VueMaterial, axios, VueAxios);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
