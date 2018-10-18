59a4ba0139b50a544988d0c13e1432dbbe1f5af6
AndrewKanaew <andrewrook-media@yandex.ru>
2018-10-18 14:19:15 +0300

    Create DashboardPage

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import LoginPage from "./views/LoginPage.vue";
+import DashboardPage from "./views/admin/DashboardPage.vue";
 
 Vue.use(Router);
 
@@ -15,18 +16,14 @@ export default new Router({
 			component: Home
 		},
 		{
+			path: "/admin",
+			name: "admin",
+			component: DashboardPage
+		},
+		{
 			path: "/login",
 			name: "login",
 			component: LoginPage
-		},
-		{
-			path: "/about",
-			name: "about",
-			// route level code-splitting
-			// this generates a separate chunk (about.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () =>
-				import(/* webpackChunkName: "about" */ "./views/About.vue")
 		}
 	]
 });
---
+++ src/views/admin/DashboardPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="md-layout">
+		<div class="md-layout-item">
+			<card title="Пользователи">
+				<md-button class="md-primary md-raised">Добавить</md-button>
+				<md-button to="/admin/users" class="md-primary">Список</md-button>
+			</card>
+		</div>
+		<div class="md-layout-item">
+			<card title="Заказы">
+				<md-button to="/admin/orders" class="md-primary md-raised">Список</md-button>
+			</card>
+		</div>
+		<div class="md-layout-item">
+			<card title="Статистика">
+				<md-button class="md-primary md-raised">Скачать</md-button>
+				<md-button to="/admin/statistics" class="md-primary">Просмотреть</md-button>
+			</card>
+		</div>
+	</div>
+</template>
+
+<script>
+import Card from "../../components/Card";
+export default {
+	name: "DashboardPage",
+	components: { Card }
+};
+</script>
+
+<style scoped>
+</style>
